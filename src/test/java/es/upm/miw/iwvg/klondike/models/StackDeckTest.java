b9c65e9e72efcdc7c6c79bac14318c26b4546a9b
Jahir <Jahir@LAPTOP-07M3ALHS>
2016-10-04 22:33:18 +0200

    Commit Class Test StackCardTest y StackDeckTest

---
+++ src/test/java/es/upm/miw/iwvg/klondike/models/StackDeckTest.java
@@ -0,0 +1,20 @@
+package es.upm.miw.iwvg.klondike.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StackDeckTest {
+	private StackDeck stackDeck;
+
+	@Before
+	public void init() {
+		stackDeck = new StackDeck();
+	}
+
+	@Test
+	public void testConstructor() {
+		assertEquals(52, stackDeck.getStackCard().size());
+	}
+}
