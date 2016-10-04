406b59793d9b5f9c8054bebb19e6fef3bc8de7a5
Jahir <Jahir@LAPTOP-07M3ALHS>
2016-10-04 21:42:50 +0200

    Archivo UML

---
+++ src/test/java/es/upm/miw/iwvg/klondike/models/GameTest.java
@@ -0,0 +1,29 @@
+package es.upm.miw.iwvg.klondike.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public void testGetDeck() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLadders() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSuitStacks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDiscard() {
+		fail("Not yet implemented");
+	}
+
+}
